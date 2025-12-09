e5fc872b59fa7105b8d28307d954e279d562db82
Dayeon Shin <jadore845@snu.ac.kr>
2025-12-09 23:51:35 +0000

    u

--- 프로교육/temp.cpp
+++ 프로교육/temp.cpp
@@ -1,3 +1,202 @@
+#include <algorithm>
+#include <vector>
+
+#define MAX_N 1000
+#define MAX_PLANTS 100001
+#define BUC_SIZE 20
+#define INF_NEG -1000000000
+
+using namespace std;
+
+int Nglobal;
+int growthTime[3];
+int plantCount;
+
+struct Plant {
+    int r, c;
+    int time;      // normalized time (includes bucket wt at sow)
+    int category;  // 0..2
+} plants[MAX_PLANTS];
+
+static const int BUC_R = (MAX_N / BUC_SIZE) + 1;
+static const int BUC_C = (MAX_N / BUC_SIZE) + 1;
+
+vector<int> buc[BUC_R][BUC_C];
+int wt[BUC_R][BUC_C]; // water per buck
+int mt_arr[BUC_R][BUC_C][3]; // 자라는데 걸리는 최대시간
+
+inline void purgeBucket(int rb, int cb) {
+    buc[rb][cb].clear();
+    wt[rb][cb] = 0;
+    for (int k = 0; k < 3; ++k) mt_arr[rb][cb][k] = INF_NEG;
+}
+
+void init(int N, int mGrowthTime[3]) {
+    Nglobal = N;
+    plantCount = 0;
+    for (int k = 0; k < 3; ++k) growthTime[k] = mGrowthTime[k];
+    for (int i = 0; i < N / BUC_SIZE; ++i) {
+        for (int j = 0; j < N / BUC_SIZE; ++j) {
+            purgeBucket(i, j);
+        }
+    }
+}
+
+int sow(int mTime, int mRow, int mCol, int mCategory) {
+    int rb = mRow / BUC_SIZE;
+    int cb = mCol / BUC_SIZE;
+
+    for (int idx : buc[rb][cb]) {
+        if (plants[idx].r == mRow && plants[idx].c == mCol) return 0;
+    }
+
+    int normalized_time = mTime + wt[rb][cb] * growthTime[mCategory];
+    plants[plantCount] = { mRow, mCol, normalized_time, mCategory };
+    buc[rb][cb].push_back(plantCount++);
+
+    mt_arr[rb][cb][mCategory] = max(mt_arr[rb][cb][mCategory], normalized_time);
+
+    return 1;
+}
+
+int bucketWater(int G, int rb, int cb, int sr, int er, int sc, int ec) {
+    int cnt = 0;
+    for (int k = 0; k < 3; ++k) mt_arr[rb][cb][k] = INF_NEG;
+
+    for (int pid : buc[rb][cb]) {
+        if (plants[pid].r >= sr && plants[pid].r <= er && plants[pid].c >= sc && plants[pid].c <= ec) {
+            plants[pid].time -= G * growthTime[plants[pid].category];
+            ++cnt;
+        }
+        mt_arr[rb][cb][plants[pid].category] = max(mt_arr[rb][cb][plants[pid].category], plants[pid].time);
+    }
+    return cnt;
+}
+
+int water(int mTime, int G, int mRow, int mCol, int mHeight, int mWidth) {
+    int ret = 0;
+    int sr = mRow, sc = mCol, er = mRow + mHeight - 1, ec = mCol + mWidth - 1;
+    int srb = sr / BUC_SIZE, erb = er / BUC_SIZE, scb = sc / BUC_SIZE, ecb = ec / BUC_SIZE;
+
+
+    for (int rb = srb + 1; rb < erb; ++rb) {
+        for (int cb = scb + 1; cb < ecb; ++cb) {
+            ret += buc[rb][cb].size();
+            wt[rb][cb] += G;
+        }
+    }
+
+
+    for (int rb = srb, cb = scb; rb <= erb; ++rb) {
+        ret += bucketWater(G, rb, cb, sr, er, sc, ec);
+    }
+    if (scb != ecb) {
+        for (int rb = srb, cb = ecb; rb <= erb; ++rb) {
+            ret += bucketWater(G, rb, cb, sr, er, sc, ec);
+        }
+    }
+    for (int rb = srb, cb = scb + 1; cb < ecb; ++cb) {
+        ret += bucketWater(G, rb, cb, sr, er, sc, ec);
+    }
+    if (srb != erb) {
+        for (int rb = erb, cb = scb + 1; cb < ecb; ++cb) {
+            ret += bucketWater(G, rb, cb, sr, er, sc, ec);
+        }
+    }
+
+    return ret;
+}
+
+bool isBucketHarvestable(int mTime, const int Lg[3], int rb, int cb, int sr, int er, int sc, int ec) {
+    for (int pid : buc[rb][cb]) {
+        if (plants[pid].r < sr || plants[pid].r > er || plants[pid].c < sc || plants[pid].c > ec) continue;
+        int cat = plants[pid].category;
+        int effective = mTime - plants[pid].time + wt[rb][cb] * growthTime[plants[pid].category];
+        if (effective < Lg[plants[pid].category]) return false;
+    }
+    return true;
+}
+
+int bucketHarvestRemove(int rb, int cb, int sr, int er, int sc, int ec) {
+    int removed = 0;
+    for (int k = 0; k < 3; ++k) mt_arr[rb][cb][k] = INF_NEG;
+
+    for (auto it = buc[rb][cb].begin(); it != buc[rb][cb].end();) {
+        int pid = *it;
+        if (plants[pid].r >= sr && plants[pid].r <= er && plants[pid].c >= sc && plants[pid].c <= ec) {
+            it = buc[rb][cb].erase(it);
+            ++removed;
+        }
+        else {
+            mt_arr[rb][cb][plants[pid].category] = max(mt_arr[rb][cb][plants[pid].category], plants[pid].time);
+            ++it;
+        }
+    }
+    return removed;
+}
+
+int harvest(int mTime, int L, int mRow, int mCol, int mHeight, int mWidth) {
+    int sr = mRow, sc = mCol;
+    int er = mRow + mHeight - 1, ec = mCol + mWidth - 1;
+
+    int srb = sr / BUC_SIZE, erb = er / BUC_SIZE;
+    int scb = sc / BUC_SIZE, ecb = ec / BUC_SIZE;
+
+    int Lg[3];
+    for (int k = 0; k < 3; ++k) Lg[k] = L * growthTime[k];
+
+    for (int rb = srb + 1; rb < erb; ++rb) {
+        for (int cb = scb + 1; cb < ecb; ++cb) {
+            for (int cat = 0; cat < 3; ++cat) {
+                if (mt_arr[rb][cb][cat] == INF_NEG) continue;
+                if (mTime - mt_arr[rb][cb][cat] + wt[rb][cb] * growthTime[cat] < Lg[cat]) return 0;
+            }
+        }
+    }
+
+    for (int rb = srb; rb <= erb; ++rb) {
+        if (!isBucketHarvestable(mTime, Lg, rb, scb, sr, er, sc, ec)) return 0;
+    }
+    if (scb != ecb) {
+        for (int rb = srb, cb = ecb; rb <= erb; ++rb) if (!isBucketHarvestable(mTime, Lg, rb, ecb, sr, er, sc, ec)) return 0;
+    }
+    for (int cb = scb + 1; cb <= ecb - 1; ++cb) {
+        if (!isBucketHarvestable(mTime, Lg, srb, cb, sr, er, sc, ec)) return 0;
+        
+    }
+    if (srb != erb) {
+        for (int rb = erb, cb = scb + 1; cb < ecb; ++cb) if (!isBucketHarvestable(mTime, Lg, erb, cb, sr, er, sc, ec)) return 0;
+    }
+    int removed = 0;
+
+    for (int rb = srb + 1; rb <= erb - 1; ++rb) {
+        for (int cb = scb + 1; cb <= ecb - 1; ++cb) {
+            removed += (int)buc[rb][cb].size();
+            purgeBucket(rb, cb);
+        }
+    }
+
+    for (int rb = srb, cb = scb; rb <= erb; ++rb) {
+        removed += bucketHarvestRemove(rb, cb, sr, er, sc, ec);
+    }
+    if (scb != ecb) {
+        for (int rb = srb, cb = ecb; rb <= erb; ++rb) {
+            removed += bucketHarvestRemove(rb, cb, sr, er, sc, ec);
+        }
+    }
+    for (int rb = srb, cb = scb + 1; cb < ecb; ++cb) {
+        removed += bucketHarvestRemove(rb, cb, sr, er, sc, ec);
+    }
+    if (srb != erb) {
+        for (int rb = erb, cb = scb + 1; cb < ecb; ++cb) {
+            removed += bucketHarvestRemove(rb, cb, sr, er, sc, ec);
+        }
+    }
+
+    return removed;
+}
+
+
 /*
 * @file: [H2529][Pro] 스마트 팜
   
